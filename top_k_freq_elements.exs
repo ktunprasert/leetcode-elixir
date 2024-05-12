c666e20c69b5376fe3e25f2014d01f0d7b70619f
ktunprasert <ktunprasert@outlook.com>
2024-05-12 11:21:07 +0100

    top_k_freq_el: implemented

--- top_k_freq_elements.exs
+++ top_k_freq_elements.exs
@@ -1,9 +1,24 @@
-nums = [1, 1, 1, 2, 2, 3]
+nums = [4, 1, -1, 2, -1, 2, 3]
 k = 2
 
 defmodule Solution do
-  def top_k_elements(nums, k) do
+  def top_k_frequent(nums, k) when length(nums) === k, do: nums
+
+  def top_k_frequent(nums, k) do
+    nums
+    |> Enum.frequencies()
+    |> Enum.sort_by(fn {_, count} -> -count end)
+    |> Enum.take(k)
+    |> Enum.map(fn {n, _} -> n end)
+    # |> Enum.uniq()
+    # |> Enum.map(fn n ->
+    #   count = Enum.count(nums, &(&1 == n))
+    #   {-count, n}
+    # end)
+    # |> Enum.sort()
+    # |> Enum.take(k)
+    # |> Enum.flat_map(fn {_, v} -> [v] end)
   end
 end
 
-Solution.top_k_elements(nums, k) |> IO.inspect()
+Solution.top_k_frequent(nums, k) |> IO.inspect()
